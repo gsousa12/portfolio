c551f767cf7a0fac26f9a58ba0363376e174835b
gsousa12 <wgsousa12@gmail.com>
2025-07-19 20:49:38 -0300

    Update project description and adjust stack colors in utils

--- src/shared/datas/project-list-datas.ts
+++ src/shared/datas/project-list-datas.ts
@@ -11,7 +11,7 @@ export const ProjectsListData: ProjectItem[] = [
   {
     name: "Rate Limit with Leaky Bucket",
     description:
-      "A rate limit implementation using the leaky bucket algorithm.",
+      "API de estudo de rate-limiting com o algoritmo Leaky Bucket, utilizando Elysia (Bun), Redis e TypeScript. Permite limitar requisições por chave de API de forma simples e rápida, com suporte a Docker e fallback automático em caso de falha no Redis.",
     stack: ["Bun", "ElysiaJs", "Redis", "TypeScript"],
     imageUrl:
       "https://miro.medium.com/v2/resize:fit:1358/1*8DxENfn29bzwA00jFUTCFA.png",
--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -6,12 +6,11 @@ export const getStackColors = (
     { color: string; labelColor: "black" | "white" }
   > = {
     TypeScript: { color: "bg-blue-400", labelColor: "white" },
-    JavaScript: { color: "bg-yellow-400", labelColor: "black" },
     React: { color: "bg-cyan-400", labelColor: "white" },
-    NodeJs: { color: "bg-green-400", labelColor: "white" },
+    NodeJs: { color: "bg-emerald-400", labelColor: "white" },
     ElysiaJs: { color: "bg-purple-400", labelColor: "white" },
     Redis: { color: "bg-red-400", labelColor: "white" },
-    Bun: { color: "bg-orange-400", labelColor: "white" },
+    Bun: { color: "bg-yellow-400", labelColor: "white" },
   };
 
   return colorMap[tech] || { color: "bg-gray-400", labelColor: "white" };
