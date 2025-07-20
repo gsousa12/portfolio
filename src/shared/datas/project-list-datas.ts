2b0c0637c8d7a49b63982864ce9c03daf709890a
gsousa12 <wgsousa12@gmail.com>
2025-07-20 10:41:11 -0300

    Add Notification System project and update tech stack colors

--- src/shared/datas/project-list-datas.ts
+++ src/shared/datas/project-list-datas.ts
@@ -9,6 +9,16 @@ export type ProjectItem = {
 
 export const ProjectsListData: ProjectItem[] = [
   {
+    name: "Notification System",
+    description:
+      "Sistema de Notificações com Garantia de Entrega. Fastify(Node) + BullMQ (Redis) + Mongo + ReactJs. Filas persistentes e retry de falhas (BullMQ) e client React com real-time via WebSockets.",
+    stack: ["NodeJs", "Fastify", "Redis", "Mongo", "React"],
+    imageUrl:
+      "https://miro.medium.com/v2/resize:fit:1400/1*V8vAkYA9QOi-gLKh68mtOg.jpeg",
+    websiteUrl: null,
+    codeUrl: "https://github.com/gsousa12/notification-system",
+  },
+  {
     name: "Rate Limit with Leaky Bucket",
     description:
       "API de estudo de rate-limiting com o algoritmo Leaky Bucket, utilizando Elysia (Bun), Redis e TypeScript. Permite limitar requisições por chave de API de forma simples e rápida, com suporte a Docker e fallback automático em caso de falha no Redis.",
--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,17 +1,20 @@
 export const getStackColors = (
   tech: string
 ): { color: string; labelColor: "black" | "white" } => {
+  const defaultValues = { color: "bg-gray-400", labelColor: "white" } as const;
   const colorMap: Record<
     string,
     { color: string; labelColor: "black" | "white" }
   > = {
     TypeScript: { color: "bg-blue-400", labelColor: "white" },
     React: { color: "bg-cyan-400", labelColor: "white" },
-    NodeJs: { color: "bg-emerald-400", labelColor: "white" },
+    NodeJs: { color: "bg-emerald-600", labelColor: "white" },
     ElysiaJs: { color: "bg-purple-400", labelColor: "white" },
+    Fastify: { color: "bg-blue-500", labelColor: "white" },
+    Mongo: { color: "bg-green-500", labelColor: "white" },
     Redis: { color: "bg-red-400", labelColor: "white" },
     Bun: { color: "bg-yellow-400", labelColor: "white" },
   };
 
-  return colorMap[tech] || { color: "bg-gray-400", labelColor: "white" };
+  return colorMap[tech] || defaultValues;
 };
